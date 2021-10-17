f95372c60e33614a3245d9668552cce63a69404f
joy-hhh <joyhhh@outlook.kr>
2021-10-17 21:05:34 +0900

    initial commit

---
+++ KOSPI.R
@@ -0,0 +1,70 @@
+if(!require(quantmod)){install.packages("quantmod");library(quantmod)}
+library(ggplot2)
+
+KOSPI <- getSymbols("^KS11",
+                   from = "2001-01-01",
+                   to = Sys.time(),
+                   auto.assign = FALSE)
+
+KOSDAQ <- getSymbols("^KQ11",
+                   from = "2001-01-01",
+                   to = Sys.time(),
+                   auto.assign = FALSE)
+
+head(KOSPI)
+tail(KOSPI)
+
+SEC <- getSymbols("005930.KS",
+                  from = "2015-01-01",
+                  to = "2021-01-01",
+                  auto.assign = FALSE)
+
+
+kq <- getSymbols("091990,KQ",
+                 from = "2015-01-01",
+                 to = "2021-01-01",
+                 auto.assign = FALSE)
+
+
+
+chartSeries(KOSPI)
+str(KOSPI)
+
+sample = data.frame(date = time(KOSPI),
+                    KOSPI,
+                    growth = ifelse(Cl(KOSPI) > Op(KOSPI), "up", "down"))
+
+?Cl
+
+colnames(KOSPI)
+colnames(sample) <- c("date", "Open", "High", "Low", "Close", "Volume", "Adjusted", "growth")
+
+summary(sample)
+
+ggplot(sample, aes(x = date)) +
+  geom_line(aes(y = Low))
+
+### KOSPI 시각화
+   
+ggplot(sample[sample$date >= "2020-01-01",], aes(x = date)) +
+  geom_linerange(aes(ymin = Low, ymax = High)) +
+  geom_rect(aes(xmin = date -0.3,
+                xmax = date +0.3,
+                ymin = pmin(Open, Close),
+                ymax = pmax(Open, Close),
+                fill = growth)) +
+  guides(fill = "none") +
+  scale_fill_manual(values = c("down" = "blue", "up" = "red"))+
+  labs(
+    title = "KOSPI",
+    subtitle = "2020-01-01 ~ 2021-10-15"
+  ) +
+  theme(plot.title = element_text(face = "bold"),
+        plot.subtitle = element_text(hjust = 1),
+        axis.title = element_blank(),
+        axis.line.x.bottom = element_line(color = "grey"),
+        axis.ticks = element_line(color = "grey"),
+        axis.line.y.left = element_line(colour = "grey"),
+        plot.background = element_rect(fill = "white"),
+        panel.background = element_rect(fill = "white"))
+
