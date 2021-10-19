c2cc3b7a31eeb31c899410e0682989489b77b5c1
joy-hhh <joyhhh@outlook.kr>
2021-10-19 17:13:29 +0900

    beta

--- KOSPI.R
+++ KOSPI.R
@@ -2,13 +2,15 @@ if(!require(quantmod)){install.packages("quantmod");library(quantmod)}
 library(ggplot2)
 
 KOSPI <- getSymbols("^KS11",
-                   from = "2001-01-01",
-                   to = Sys.time(),
+                   from = "2016-01-01",
+                   to = "2020-12-31",
+                   # to = Sys.time(),
                    auto.assign = FALSE)
 
 KOSDAQ <- getSymbols("^KQ11",
-                   from = "2001-01-01",
-                   to = Sys.time(),
+                   from = "2016-01-01",
+                   to = "2020-12-31",
+                   # to = Sys.time(),
                    auto.assign = FALSE)
 
 head(KOSPI)
@@ -34,7 +36,6 @@ sample = data.frame(date = time(KOSPI),
                     KOSPI,
                     growth = ifelse(Cl(KOSPI) > Op(KOSPI), "up", "down"))
 
-?Cl
 
 colnames(KOSPI)
 colnames(sample) <- c("date", "Open", "High", "Low", "Close", "Volume", "Adjusted", "growth")
@@ -67,4 +68,58 @@ ggplot(sample[sample$date >= "2020-01-01",], aes(x = date)) +
         axis.line.y.left = element_line(colour = "grey"),
         plot.background = element_rect(fill = "white"),
         panel.background = element_rect(fill = "white"))
+
+
+
+library(tqk)
+code <- code_get()
+code
+sscode <- code[grep("^삼성전자$", code$name),3]
+sscode
+samsung <- tqk_get(sscode, from="2021-01-01")
+samsung
+
+kscode <- "^KS11"
+KS11 <- tqk_get(kscode, from="2021-01-01")
+
+
+
+## 파이프 (%>%) 사용 with dplyr
+library(dplyr)
+code_get() %>% 
+  filter(grepl("^삼성전자$", name)) %>% 
+  select(code) %>% 
+  tqk_get(from = "2016-01-01") -> ss
+ss <- ss %>% filter(date <= "2020-12-31")
+ss$Stock <- ss$adjusted/lag(ss$adjusted) - 1
+names(stm) <-c("Stock")
+
+dates <- index(KOSPI)
+KOSPI <- as_tibble(KOSPI)
+KOSPI <- KOSPI %>% mutate(date = dates)
+KOSPI <- KOSPI %>% mutate(ks = KS11.Adjusted /lag(KS11.Adjusted) - 1)
+
+dates <- index(KOSDAQ)
+KOSDAQ <- as_tibble(KOSDAQ)
+KOSDAQ <- KOSDAQ %>% mutate(date = dates)
+KOSDAQ <- KOSDAQ %>% mutate(ks = KS11.Adjusted /lag(KS11.Adjusted) - 1)
+
+
+
+test <- KOSPI %>% select(date, KS11.Adjusted)
+test_df <- left_join(ss, test)
+test_df %>% filter(is.na(test_df$KS11.Adjusted))
+
+start_date <- '2016-01-01'
+end_date <- '2020-12-31'
+
+library(reticulate)
+fdr <- import("FinanceDataReader")
+KOSPI <-　fdr$DataReader('ks11', start_date, end_date)                  
+KOSPI <- KOSPI %>% mutate(rate = (Close /lag(Close) - 1))
+samsung <-　fdr$DataReader('005930', start_date, end_date)                  
+samsung <- samsung %>% mutate(rate = (Close /lag(Close) - 1))
+Coeff <- lm(samsung$rate ~ KOSPI$rate)
+Coeff[[1]][[2]]
+
 
